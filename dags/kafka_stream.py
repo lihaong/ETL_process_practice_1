d5450e59bf17fafa9b1133741fbeb3306674c392
Muhammad Fajar Andikha <54676003+lihaong@users.noreply.github.com>
2024-10-31 04:12:21 +0000

    add docker-compose.yml

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -20,13 +20,29 @@ def get_data():
 def format_data(res):
     data = {}
     location = res['location']
+    # data['id'] = uuid.uuid4()
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
-    data['address'] = res['address']
-    
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
 
-# def stream_data():
+    return data
+
+def stream_data():
+    import json
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=3))
+
+
     
 with DAG('user_automation',
     default_args=default_args,
